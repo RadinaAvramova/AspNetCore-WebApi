ddf964ca4f3c0fa8a35be80c077d4f8a50a96bf6
Radina Avramova <99686592+RadinaAvramova@users.noreply.github.com>
2024-03-04 14:16:52 +0100

    Add files via upload

---
+++ Services/DataInitializer/UserDataInitializer.cs
@@ -0,0 +1,35 @@
+﻿using Data.Repositories;
+using Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Services.DataInitializer
+{
+    public class UserDataInitializer : IDataInitializer
+    {
+        private readonly UserManager<User> userManager;
+
+        public UserDataInitializer(UserManager<User> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        public void InitializeData()
+        {
+            if (!userManager.Users.AsNoTracking().Any(p => p.UserName == "Admin"))
+            {
+                var user = new User
+                {
+                    Age = 25,
+                    FullName = "محمد جوادابراهیمی",
+                    Gender = GenderType.Male,
+                    UserName = "admin",
+                    Email = "admin@site.com"
+                };
+                var result = userManager.CreateAsync(user, "123456").GetAwaiter().GetResult();
+            }
+        }
+    }
+}
