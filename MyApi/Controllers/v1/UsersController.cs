92e46a7e60cc4f5e123e8c444dff92c0693eeac5
Radina Avramova <99686592+RadinaAvramova@users.noreply.github.com>
2024-05-11 21:06:03 +0300

    Update UsersController.cs

--- MyApi/Controllers/v1/UsersController.cs
+++ MyApi/Controllers/v1/UsersController.cs
@@ -1,4 +1,4 @@
-﻿using Common.Exceptions;
+using Common.Exceptions;
 using Data.Repositories;
 using ElmahCore;
 using Entities;
@@ -47,13 +47,6 @@ namespace MyApi.Controllers.v1
         [Authorize(Roles = "Admin")]
         public virtual async Task<ActionResult<List<User>>> Get(CancellationToken cancellationToken)
         {
-            //var userName = HttpContext.User.Identity.GetUserName();
-            //userName = HttpContext.User.Identity.Name;
-            //var userId = HttpContext.User.Identity.GetUserId();
-            //var userIdInt = HttpContext.User.Identity.GetUserId<int>();
-            //var phone = HttpContext.User.Identity.FindFirstValue(ClaimTypes.MobilePhone);
-            //var role = HttpContext.User.Identity.FindFirstValue(ClaimTypes.Role);
-
             var users = await userRepository.TableNoTracking.ToListAsync(cancellationToken);
             return Ok(users);
         }
@@ -61,44 +54,32 @@ namespace MyApi.Controllers.v1
         [HttpGet("{id:int}")]
         public virtual async Task<ApiResult<User>> Get(int id, CancellationToken cancellationToken)
         {
-            var user2 = await userManager.FindByIdAsync(id.ToString());
+            var user = await userManager.FindByIdAsync(id.ToString());
             var role = await roleManager.FindByNameAsync("Admin");
 
-            var user = await userRepository.GetByIdAsync(cancellationToken, id);
-            if (user == null)
+            var retrievedUser = await userRepository.GetByIdAsync(cancellationToken, id);
+            if (retrievedUser == null)
                 return NotFound();
 
-            await userManager.UpdateSecurityStampAsync(user);
-            //await userRepository.UpdateSecuirtyStampAsync(user, cancellationToken);
+            await userManager.UpdateSecurityStampAsync(retrievedUser);
 
-            return user;
+            return retrievedUser;
         }
 
-        /// <summary>
-        /// This method generate JWT Token
-        /// </summary>
-        /// <param name="tokenRequest">The information of token request</param>
-        /// <param name="cancellationToken"></param>
-        /// <returns></returns>
         [HttpPost("[action]")]
         [AllowAnonymous]
-        public virtual async Task<ActionResult> Token([FromForm]TokenRequest tokenRequest, CancellationToken cancellationToken)
+        public virtual async Task<ActionResult> Token([FromForm] TokenRequest tokenRequest, CancellationToken cancellationToken)
         {
             if (!tokenRequest.grant_type.Equals("password", StringComparison.OrdinalIgnoreCase))
                 throw new Exception("OAuth flow is not password.");
 
-            //var user = await userRepository.GetByUserAndPass(username, password, cancellationToken);
             var user = await userManager.FindByNameAsync(tokenRequest.username);
             if (user == null)
-                throw new BadRequestException("نام کاربری یا رمز عبور اشتباه است");
+                throw new BadRequestException("Invalid username or password");
 
             var isPasswordValid = await userManager.CheckPasswordAsync(user, tokenRequest.password);
             if (!isPasswordValid)
-                throw new BadRequestException("نام کاربری یا رمز عبور اشتباه است");
-
-
-            //if (user == null)
-            //    throw new BadRequestException("نام کاربری یا رمز عبور اشتباه است");
+                throw new BadRequestException("Invalid username or password");
 
             var jwt = await jwtService.GenerateAsync(user);
             return new JsonResult(jwt);
@@ -108,13 +89,8 @@ namespace MyApi.Controllers.v1
         [AllowAnonymous]
         public virtual async Task<ApiResult<User>> Create(UserDto userDto, CancellationToken cancellationToken)
         {
-            logger.LogError("متد Create فراخوانی شد");
-            HttpContext.RiseError(new Exception("متد Create فراخوانی شد"));
-
-            //var exists = await userRepository.TableNoTracking.AnyAsync(p => p.UserName == userDto.UserName);
-            //if (exists)
-            //    return BadRequest("نام کاربری تکراری است");
-
+            logger.LogError("Create method called");
+            HttpContext.RiseError(new Exception("Create method called"));
 
             var user = new User
             {
@@ -124,17 +100,17 @@ namespace MyApi.Controllers.v1
                 UserName = userDto.UserName,
                 Email = userDto.Email
             };
+
             var result = await userManager.CreateAsync(user, userDto.Password);
 
-            var result2 = await roleManager.CreateAsync(new Role
+            await roleManager.CreateAsync(new Role
             {
                 Name = "Admin",
                 Description = "admin role"
             });
 
-            var result3 = await userManager.AddToRoleAsync(user, "Admin");
+            await userManager.AddToRoleAsync(user, "Admin");
 
-            //await userRepository.AddAsync(user, userDto.Password, cancellationToken);
             return user;
         }
 
@@ -166,3 +142,4 @@ namespace MyApi.Controllers.v1
         }
     }
 }
+
